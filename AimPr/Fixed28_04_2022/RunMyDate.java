40f4032ab0c35e337b8bd851a8d5d3378c1cf403
Rippo Kazami <90459878+Rippo12@users.noreply.github.com>
2022-04-28 15:54:40 +0700

    Add files via upload

---
+++ AimPr/Fixed28_04_2022/RunMyDate.java
@@ -0,0 +1,12 @@
+
+public class RunMyDate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyDate a= new  MyDate();
+		a.acceptDate();
+		a.printDate();
+	}
+
+}
+
